6593eaff57db4a8e06aa78c88de651e48cc14a34
anxoCastro <anxoc98@gmail.com>
2022-03-29 22:56:28 +0200

    Solucionada implementación negamax

--- search.c
+++ search.c
@@ -9,7 +9,7 @@
 #include "moves.h"
 unsigned long long nodes;
 move best_move;
-
+int ply;
 int negamax(board *b, int alpha, int beta, int depth, move lastMove){
     if(depth == 0){
         return evaluation(*b);
@@ -21,6 +21,8 @@ int negamax(board *b, int alpha, int beta, int depth, move lastMove){
     //Inicializar lista de movimientos
     moveList m;
     m.nElements = 0;
+    move best_sofar;
+    int old_alpha = alpha;
 
     if(b->side == WHITE){
         generate_white_moves(b, lastMove, &m);
@@ -32,13 +34,15 @@ int negamax(board *b, int alpha, int beta, int depth, move lastMove){
     }
 
     for(int i = 0; i< m.nElements; i++){
+        ply++;
         if(make_legal_move(b, m.list[i]) == 1){
+            ply--;
             continue;
         }
         lastMove = m.list[i];
 
         int score = -negamax(b, -beta, -alpha, depth - 1, lastMove);
-
+        ply--;
         unmake_move(b, m.list[i]);
         legal_moves++;
         //Poda beta
@@ -48,6 +52,9 @@ int negamax(board *b, int alpha, int beta, int depth, move lastMove){
 
         if(score > alpha){
             alpha = score;
+
+            if(ply==0)
+                best_sofar = lastMove;
         }
     }
 
@@ -61,69 +68,22 @@ int negamax(board *b, int alpha, int beta, int depth, move lastMove){
             return 0;
         }
     }
-
+    // found better move
+    if (old_alpha != alpha)
+        // init best move
+        best_move = best_sofar;
     return alpha;
 
 }
 
-int do_negamax(board *b, int depth, move lastMove){
-    //Inicializar lista de movimientos
-    moveList m;
-    m.nElements = 0;
-    //-infinito +infinito
-    int alpha = -50000;
-    int beta = 50000;
-    int legal_moves = 0;
-    int in_check;
-    if(b->side == WHITE){
-        generate_white_moves(b, lastMove, &m);
-        in_check = is_attacked(b, get_ls1b_index(b->WK), WHITE);
-    }
-    else{
-        generate_black_moves(b, lastMove, &m);
-        in_check = is_attacked(b, get_ls1b_index(b->BK), BLACK);
-    }
-    for(int i = 0; i< m.nElements; i++){
-        if(make_legal_move(b, m.list[i]) == 1){
-            continue;
-        }
-        lastMove = m.list[i];
-        int score = -negamax(b, -beta, -alpha, depth - 1, lastMove);
-        unmake_move(b, m.list[i]);
-        legal_moves++;
-        //Poda beta
-        if(score >= beta){
-            return beta;
-        }
-
-        if(score > alpha){
-            alpha = score;
-            best_move = m.list[i];
-        }
-    }
-
-    if(legal_moves == 0){
-        if(in_check){
-            //Jaque mate
-            return -49000 - depth;
-        }
-        else{
-            //GG
-            return 0;
-        }
-    }
-
-}
-
-
 move search_position(board *b, int depth, move lastMove){
     nodes = 0;
     initMove(&best_move);
-    do_negamax(b, depth, lastMove);
+    negamax(b, -50000, 50000, depth, lastMove);
 
     char *string_move = malloc(sizeof(char) * 5);
     move_to_string(&best_move, string_move);
     printf("bestmove %s\n", string_move);
-
+    free(string_move);
     return best_move;
 }
